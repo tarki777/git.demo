266fad0e8fd32a7b40b2d4387732532eb8a7aaf6
tarki777 <154429590+tarki777@users.noreply.github.com>
2025-02-25 13:25:33 +0300

    first  commit

--- src/main/java/com/ithub/test_project/Controller.java
+++ src/main/java/com/ithub/test_project/Controller.java
@@ -3,6 +3,7 @@ package com.ithub.test_project;
 public class Controller {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println(" hello " );
     }
 
 }
